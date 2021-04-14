6a9ae1a5628224724729cafa2fb14abf55c8a88e
Alessandra Brugneroto <56523370+alessandrabrugneroto@users.noreply.github.com>
2021-04-14 15:29:24 -0300

    Update chiquifaseum.js

--- chiquifaseum.js
+++ chiquifaseum.js
@@ -9,16 +9,16 @@ function fasechiquinha() {
 
         document.write(`<div class="texto">
          <h1> <p> <strong> Você está na fase 1!<br> A Chiquinha pegou o patinete antes de escurecer  passou para proxima fase<br> ela terá 10 minutos para recolher todos os itens e as vezes no caminho aparece o Kiko <br>ele tem inveja da chiquinha, se ele encontrar com a chiquinha rouba o objeto dela<br>então ela tera que pegar novamente o objeto e perde tempo no jogo se a chiqinha<br> conseguir pegar o pirulito primeiro ganha mais 10 minutos no jogo para as proximas fases</h1></div>
-          <p> <a href ="file:///C:/Projeto_moduloI/chiquinhafasedois.html"><button><strong> Proxima fase</strong></button></a> </p>
-           <p> <a href = "file:///C:/Projeto_moduloI/gameo.html"><button><strong> Desistir do desafio</strong></button> </a></p>
+          <p> <a href ="chiquinhafasedois.html"><button><strong> Proxima fase</strong></button></a> </p>
+           <p> <a href = "gameo.html"><button><strong> Desistir do desafio</strong></button> </a></p>
 
             `)
     } else if (escolha == 2) {
 
         document.write(`<div class="texto">
            <h1> <p><strong> Chiquinha ficou de castigo  acabou seu tempo!</strong></h1></div>
-             <a href = "file:///C:/Projeto_moduloI/gameo.html" ><button> Não quero desafio</button></a>
-             <a href="file:///C:/Projeto_moduloI/docinicio.html"><button>Quero um novo desafio</button></a> `)
+             <a href = "gameo.html" ><button> Não quero desafio</button></a>
+             <a href="3index.html"><button>Quero um novo desafio</button></a> `)
 
     } else {
         alert("Seguindo");
