b79d933b3a369be316e5ef5de68c516c694cf4cc
Alessandra Brugneroto <56523370+alessandrabrugneroto@users.noreply.github.com>
2021-04-14 15:26:28 -0300

    Update chiquifasedois.js

--- chiquifasedois.js
+++ chiquifasedois.js
@@ -10,16 +10,16 @@ function faseChiquidois() {
         document.write(`<div class="texto">
          <h1> <p> <strong>Você está na fase 2!<br> Aceitou o convite  da dona Clotilde e foi envenenada<br> perdeu 10 min de jogo agora terá 5 min para recolher todos os itens<br>o kiko disse  que o frango está envenenado pela bruxa do 71<br> mas sabemos que o Kiko menti muito, nessa fase o frango vale 10 minuntos<br> como bonos para as proximas fases</h1></div>
 
-               <p> <a href ="file:///C:/Projeto_moduloI/chiquinhafase3.html"><button><strong> Proxima fase</strong></button></a></p>
-                <a href = "file:///C:/Projeto_moduloI/gameo.html"><button><strong> Desistir do desafio</strong> </button></a>
+               <p> <a href ="chiquinhafase3.html"><button><strong> Proxima fase</strong></button></a></p>
+                <a href = "gameo.html"><button><strong> Desistir do desafio</strong> </button></a>
             `)
 
     } else if (escolha == 2) {
 
         document.write(`<div class="texto">
           <h1>  <p> Se livrou do feitiço da bruxa! passou para fase 3!</p><h/1></div>
-           <p> <a href = "file:///C:/Projeto_moduloI/chiquinhafase3.html"> <button><strong>Proxima fase</strong></button></a></p>
-           <p> <a href="file:///C:/Projeto_moduloI/gameo.html"><button><strong>Não quero novo desafio</strong></button></a></p> `)
+           <p> <a href = "chiquinhafase3.html"> <button><strong>Proxima fase</strong></button></a></p>
+           <p> <a href="gameo.html"><button><strong>Não quero novo desafio</strong></button></a></p> `)
 
     } else {
         alert("Seguindo");
