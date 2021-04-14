2993c27d809b7396f9a4adb426445fb68b01487d
Alessandra Brugneroto <56523370+alessandrabrugneroto@users.noreply.github.com>
2021-04-14 15:25:25 -0300

    Update chiquifase3.js

--- chiquifase3.js
+++ chiquifase3.js
@@ -9,14 +9,14 @@ function faseChiquitres() {
 
         document.write(`<div class="texto">
          <h1> <strong><p>Parabéns você ganhou jogo!<br> Obedeceu e se livrou dos espiritos zombeteiros que assombra a  vila<br> ganhou 10 min de bonos para proxima fase <h1></div>
-         <p>  <br> <a href ="file:///C:/Projeto_moduloI/docpers.html"><button><strong>Escolha outro personagem para o proximo desafio</strong></button></a></p>
-            <a href = "file:///C:/Projeto_moduloI/gameo.html"><button><strong> Desistir do desafio</strong></button></a></p>
+         <p>  <br> <a href ="3index.html"><button><strong>Escolha outro personagem para o proximo desafio</strong></button></a></p>
+            <a href = "gameo.html"><button><strong> Desistir do desafio</strong></button></a></p>
             `)
 
     } else if (escolha == 2) {
 
         document.write(`<div class="texto">
-            <p> Saiu a noite e encontrou os espiritos zombeiteiros ! O seu jogo acabou não tem chororo!!! <br></div> <div class=" button"><a href = "file:///C:/Projeto_moduloI/docinicio.html"><button><strong>Escolha outro personagem para o proximo desafio</strong></button></a> <br> <a href="file:///C:/Projeto_moduloI/gameo.html"><button><strong>Não quero novo desafio</strong></button></a>`)
+            <p> Saiu a noite e encontrou os espiritos zombeiteiros ! O seu jogo acabou não tem chororo!!! <br></div> <div class=" button"><a href = "3index.html"><button><strong>Escolha outro personagem para o proximo desafio</strong></button></a> <br> <a href="file:///C:/Projeto_moduloI/gameo.html"><button><strong>Não quero novo desafio</strong></button></a>`)
 
     } else {
         alert("Seguindo");
