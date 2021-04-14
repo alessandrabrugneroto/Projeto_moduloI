6849709c3bab92e10fd4dfff247b4f373ffa7a67
Alessandra Brugneroto <56523370+alessandrabrugneroto@users.noreply.github.com>
2021-04-14 15:13:02 -0300

    Update sr.madruga1.js

--- sr.madruga1.js
+++ sr.madruga1.js
@@ -9,15 +9,15 @@ function faseumMadruga() {
 
         document.write(`<div class="texto">
                 <h1><strong><p>Você está na fase 1!<br>Sr Madruga pagou o aluguel e se livrou da assombração <br>na vila se tornou muito perigoso  a lanterna pode apagar a qualquer momento<br>lembre - se a pilha dura pouco  Sr Madruga terá 15 minutos<br> para recolher todos os itens e fazer os churros</p></h1></div>
-               <p><a href="file:///C:/Projeto_moduloI/Sr.madrugafase2.html"><button><strong>Proxima fase</strong></button></a></p>
-                <p><a href="file:///C:/Projeto_moduloI/gameo.html"><button><strong>Desistir do desafio</strong></button></a></p>
+               <p><a href="Sr.madrugafase2.html"><button><strong>Proxima fase</strong></button></a></p>
+                <p><a href="gameo.html"><button><strong>Desistir do desafio</strong></button></a></p>
 
                 `)
     } else if (escolha == 2) {
 
         document.write(`<div class="texto"><h1><strong><p>Você está na fase 1! <br>Sr Madruga não pagou o aluguel,não tem chororo!<br>terá apenas 5 min para vender os churros ele precisa<br> juntar o dinheiro para pagar o aluguel antes que escureça <br> </h1> </div>
-        <p> <a href="file:///C:/Projeto_moduloI/Sr.madrugafase2.html"><button><strong>Proxima fase</strong></button></a></p>
-       <p><a href="file:///C:/Projeto_moduloI/gameo.html"><button><strong>Não quero novo desafio</strong></button></a></strong></p>`)
+        <p> <a href="Sr.madrugafase2.html"><button><strong>Proxima fase</strong></button></a></p>
+       <p><a href="gameo.html"><button><strong>Não quero novo desafio</strong></button></a></strong></p>`)
 
 
     } else {
